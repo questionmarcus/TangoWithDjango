e82ccb3d04609b377ba28453663e18af2f58d4a1
Marcus Lancaster <marcus.h.lancaster@gmail.com>
2018-01-16 15:45:59 +0000

    fixed issue with url mappings, and added about page. End of chapter 3

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says \"Hey there, partner!\"")
+
+def about(request):
+    return HttpResponse(
+        "Rango says \"Here is the about page\". "+
+        "<a href='/rango/'>Back Home</a>"
+    )
