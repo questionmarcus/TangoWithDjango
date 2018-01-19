f47a07eae0c11d298243b2945f4c1e10f1115e43
Marcus Lancaster <marcus.h.lancaster@gmail.com>
2018-01-19 13:20:48 +0000

    Set up first template and to deploy on the /rango page

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says \"Hey there, partner!\"")
+    # Construct a dict to pass to the template engine
+    # This dict tell the template what value should be placed in
+    # {{ boldmessage }}
+
+    # Return a rendered response to send to the client
+    # render function takes a request, the template filename and the context
+    context_dict = {'boldmessage': "The Ting Goes Skraaaa"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse(
